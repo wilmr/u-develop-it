403824d01aa68370669fd8daa1bbb1215b2633a6
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-11-01 19:34:18 -0700

    connected mysql db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		// Your MySQL username,
+		user: 'root',
+		// Your MySQL password
+		password: 'Peepopog69$',
+		database: 'election',
+	},
+	console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
